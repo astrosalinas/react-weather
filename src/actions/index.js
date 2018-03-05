3631f30a4ee07e5b6f33b7809a463ec9bffb5d81
astrosalinas <astrosalinas35@gmail.com>
2018-03-05 08:46:01 -0300

    incorporacion de middleware tunck

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,32 @@
+import transformForecast from "../services/transformForecast";
+
 export const SET_CITY = 'SET_CITY';
+
+const api_key = "43f3457cd7c09b1bddcd95637fd2f142";
+const url = "http://api.openweathermap.org/data/2.5/forecast";
 
 //esto es una accion creator
 export const setCity = payload => ({ type: SET_CITY, payload });
+
+//accion de fetch de updateCity
+export const fetchForecast = payload => {
+
+    return dispatch => {
+        //fetch o axios
+        const url_forecast = `${url}?q=${this.props.city}&appid=${api_key}`
+
+        //activar en el estado un indicador de busqueda de datos
+
+        fetch(url_forecast).then( //obtiene la info que viene del server
+            data => (data.json()) // data.json obtiene el objeto ya ejor formado
+        ).then(
+            weather_data => { //obtiene el weather data que es un objeto del objeto data.json
+                const forecastData = transformForecast(weather_data);
+                console.log(forecastData);
+
+                // modificar el estado con el resultado de la promise (fetch)
+            }
+        );
+        return;
+    };
+};
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,14 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import { city } from './../reducers/city';
 
 const initialState= {
     city: 'Ciudad del Este,py'
 };
 
+const composeEnhancers = window.__REDUX_DEV_TOOLS_EXTENSION_COMPOSE__ || compose;
 
-export const store = createStore(city, initialState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+export const store = createStore(city, initialState, composeEnhancers(applyMiddleware(thunk)));
+
+//si no usaramos la app redux en el navegador quedaria asi
+//export const store = createStore(city, initialState, applyMiddleware(thunk));
